3c3ad9b1ebf3ce21ee34c5c3f17fc85b57ce2f14
Ruzan Valeev <ruzan.valeeff@yandex.ru>
2023-10-23 14:23:41 +0300

    Complete test

--- data.sql
+++ data.sql
@@ -1,17 +1,59 @@
-    insert into activitytype(
-    	  activitytypeid
-        ,name
-        ,sysname
-    )
-    select d.*
-    from (
-    	values(1,'Программа','Program')
-        ,(2,'Подпрограмма','SubProgram')
-        ,(3,'Проект','Project')
-        ,(4,'Контракт','Contract')
-        ,(5,'КТ','Point')
-    ) d(activitytypeid, name, sysname)
-    where not exists(
-    	select 1 from activitytype t
-        where t.activitytypeid = d.activitytypeid
-    );
+insert into activitytype
+select d.* from (values 
+	(1,'Программа', 	'program'	),
+   	(2,'Подпрограмма',	'subprogram'),
+	(3,'Проект',		'project'	),
+	(4,'Контракт',		'contract'	),
+	(5,'КТ',			'point'		)
+) as d(id, name, sysname)
+where not exists(
+    select 1 from activitytype as t
+    where t.id = d.id
+);
+
+insert into program 
+values 	(1, 'First', 'First test program (continue)'),
+	   	(2, 'Second', 'Second test program (end)')
+on conflict (id) do nothing;
+
+insert into subprogram 
+values	(1, 'First', 'First test subprogram'),
+		(2, 'Second', 'Second test subprogram')
+on conflict (id) do nothing;
+
+insert into area 
+values	(1, 'First'),
+		(2, 'Second')
+on conflict (id) do nothing;
+
+insert into project
+values 	(1, 1, 'First', 'First test project in First area'),
+		(2, 2, 'Second', 'Second test project in Second area')
+on conflict (id) do nothing;
+
+insert into contract 
+values 	(1, 'First', 'First test contract'),
+		(2, 'Second', 'Second test contract')
+on conflict (id) do nothing;
+
+insert into point
+values	(1, 'First', 'First test checkpoint'),
+		(2, 'Second', 'Second test checkpoint')
+on conflict (id) do nothing;
+
+insert into point_parent 
+values 	(1, null, 1),
+		(2, 2, null)
+on conflict do nothing;
+
+insert into activity 
+values	(1, null, 1, 1),
+		(2, 1, 2, 1),
+		(3, 2, 3, 1),
+		(4, 3, 4, 1),
+		(5, 4, 5, 1),
+		(6, 1, 2, 2),
+		(7, 2, 3, 2),
+		(8, 3, 4, 2),
+		(9, 4, 5, 2)
+on conflict do nothing;
--- tables.sql
+++ tables.sql
@@ -1,11 +1,82 @@
+/*
+ * for debug purposes
+drop table if exists activity;
+drop table if exists activitytype;
+drop table if exists point_parent;
+drop table if exists point;
+drop table if exists contract;
+drop table if exists project;
+drop table if exists area;
+drop table if exists subprogram;
+drop table if exists program;
+*/
 
-create table if not exists activitytype
-(
-    activitytypeid integer not null
-        constraint pk_activitytype
-            primary key,
-    name           text    not null,
-    sysname        text    not null
+create table activitytype (
+    id 		integer primary key,
+    "name"	varchar not null,
+    sysname varchar not null	
 );
 
--- 
+create table activity (
+	id				integer primary key,
+	parent_id 		integer references activity (id),
+	activitytype_id	integer not null references activitytype (id),
+	activity_id		integer not null
+);
+
+create table program (
+	id		integer primary key,
+	"name"	varchar not null default 'Unknown',
+	"desc"	varchar not null default 'Unspecified'
+);
+
+
+create table subprogram (
+	id		integer primary key,
+	"name"	varchar not null default 'Unknown',
+	"desc"	varchar not null default 'Unspecified'
+);
+
+create table area (
+	id		integer primary key,
+	"name"	varchar not null default 'Unknown'
+);
+
+create table project (
+	id		integer primary key,
+	area_id	integer not null references area (id),
+	"name"	varchar not null default 'Unknown',
+	"desc"	varchar not null default 'Unspecified'
+);
+
+create table contract (
+	id		integer primary key,
+	"name"	varchar not null default 'Unknown',
+	"desc"	varchar not null default 'Unspecified'
+);
+
+create table point (
+	id			integer primary key,
+	"name"		varchar not null default 'Unknown',
+	"desc"		varchar not null default 'Unspecified'
+);
+
+-- для реализации связки кт -> (контракт ИЛИ проект)
+--
+-- при чём кт может быть связана с контрактом И проектом
+-- одновременно, потому что логично, что прохождение кт, 
+-- в некоторых случаях, может зависеть от нескольких факторов
+create table point_parent (
+	id			integer references point (id),
+	contract_id integer,
+	project_id	integer,
+	check (contract_id is not null or project_id is not null)
+);
+
+
+
+
+
+
+
+
